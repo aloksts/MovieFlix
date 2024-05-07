c74b708f7be3c8d2e0670a4324fc8ab547048d62
aloksts <l.alok.s.ab@gmail.com>
2024-05-07 17:03:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="text-3xl font-bold text-green">
-              <h1>Lets build movieflix</h1>
+              <h1>Lets build movieflix sss</h1>
+              <h2>  jai mahakal</h2>
     </div>
   );
 }
