31dd74e99f651b8cf4912a3120f09ea95b7fa4af
aloksts <l.alok.s.ab@gmail.com>
2024-05-08 20:46:55 +0530

    added signUp ()=>{}

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useState } from 'react';
 import Header from './Header';
 
 const Login = () => {
-
-   
-  return (
+   const [isSignIn, setissignIn]=useState(true);
+   const toggleSignInForm=()=>{
+       setissignIn(!isSignIn);
+   }
+   return(
     <div>
          <Header/>
 
@@ -12,14 +15,18 @@ const Login = () => {
     <img src="https://assets.nflxext.com/ffe/siteui/vlv3/d253acf4-a1e2-4462-a416-f78802dc2d85/f04bf88c-f71c-4d02-82ed-adb870b8f8db/IN-en-20240429-POP_SIGNUP_TWO_WEEKS-perspective_WEB_658a042e-62cf-473d-8da0-7b875f23e2ef_small.jpg" alt="bg"/>
     </div>
     <form className='w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80'>
-    <h1 className='text-3xl text-wt py-4'>Sign In</h1>
+    <h1 className='text-3xl text-wt py-4'>{isSignIn?"Sign In":"Sign Up"}</h1>
+    {!isSignIn && <input type="text" placeholder='Full Name' className='p-2 my-4 w-full bg-slate-500'/>}
+
     <input type="text" placeholder='email or phone number' className='p-2 my-4 w-full bg-slate-500'/>
     <input type="password" placeholder='password' className='p-2 my-4 w-full  bg-slate-500'/>
-    <button type="submit" className='p-4 my-6 bg-red-800 w-full rounded-lg'>Sign In</button>
+    <button type="submit" className='p-4 my-6 bg-red-800 w-full rounded-lg'>{isSignIn?"Sign In":"Sign Up"}</button>
+    <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignIn?"New to MovieFlix? SignUp now":"Already a User ? Sign In now"}</p>
+
     </form>
     </div>
 
-  );
+   );
 }
 
 export default Login;
